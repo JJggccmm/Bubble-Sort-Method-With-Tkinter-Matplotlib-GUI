7f197da934b5eafcdf3cbbba22c9f3f12feb6538
Felipe Alves <103237188+ffelipealves@users.noreply.github.com>
2022-10-21 10:21:13 -0300

    melhorando e testando bubblesort

--- bubblesort_Tk_Matplot_GUI.py
+++ bubblesort_Tk_Matplot_GUI.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import matplotlib.pyplot as plt
+import numpy as np
     
 ##### IMPLEMENTACAO BASICA DO BUBBLE SORT #####
 def BubbleSort(lista):
@@ -8,14 +9,34 @@ def BubbleSort(lista):
   troca=True
   x=0
   
+  #adicionamos um tamanho para diminuir
+  tamanho=len(lista)-1
+  
   while troca==True:
     troca=False
     #Utilizarei um for para percorrer a lista
-    for x in range (lista):
+    for x in range (tamanho):
       #Depois vamos fazer as comparaçoes dois a dois
       if lista[x] > lista[x+1]:
         lista[x+1],lista[x]=lista[x],lista[x+1]
         troca=troca+1
         troca=True
     #Podemos otimizar o algoritmo diminuindo o numero de comparaçoes a cada iteracao do for
+    tamanho=tamanho-1
    
+#Testando funcionalidade do bubblesort
+teste = np.array([4,5,10,20])
+
+for x in range(len(teste)):
+  
+  lista = np.random.randint(30,size=(teste[x]))
+  
+  print("Lista ANTES da ordenacao."teste[x],"Elementos:")
+  print(lista)
+  
+  BubbleSort(lista)
+  
+  print("Lista DEPOIS da ordenacao."teste[x],"Elementos:")
+  print(lista)
+  
+  
